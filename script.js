fa73f299e2e3ff288098f8394c965439a2063a16
Nehul Sukralia <nehul@binmile.com>
2021-11-15 16:18:42 +0530

    modified contanier

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const onClick = (e) => {
   const years = document.querySelector(".Ye").value;
   console.log(amount);
   let total = amount * Math.pow(1 + interest / 100, years);
-  result.innerHTML = "Total amount is " + total.toFixed(2);
+  result.innerHTML = "Total amount is $" + total.toFixed(2);
 };
 
 button.addEventListener("click", onClick);
